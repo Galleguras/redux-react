98a00e1e0176560492078ac6b356887a89278af9
Pablo Gallego Carmona <pablogallegocarmona@gmail.com>
2021-03-11 08:36:40 +0100

    primera parte

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux'
+import {createStore,combinereducers} from'redux'
 
-export default function App() {
-  return (
+const reducers ={
+  todos: (state = []) => {
+    return state
+  }
+}
+
+const MyApp =() => {
+  return(
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+const store = createStore(combinereducers({...reducers}))
+
+export default function App() {
+  return (
+ <Provider store={store}>
+      <MyApp/>
+ </Provider>
   );
 }
 
